8778a35b99a08e9fa1e39c3570dcad6170ea9ebb
RYO <ryo@FL1-118-111-38-220.tky.mesh.ad.jp>
2015-04-06 12:38:29 +0900

    Initial Commit

---
+++ DogCatch(ver1.01)Tests/DogCatch_ver1_01_Tests.m
@@ -0,0 +1,40 @@
+//
+//  DogCatch_ver1_01_Tests.m
+//  DogCatch(ver1.01)Tests
+//
+//  Created by RYO on 2015/04/06.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DogCatch_ver1_01_Tests : XCTestCase
+
+@end
+
+@implementation DogCatch_ver1_01_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

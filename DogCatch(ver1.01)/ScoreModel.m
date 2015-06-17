3bc53dfb62d80b9860b977460c637986f52b5b46
RYO <miniature-dachs@excite.co.jp>
2015-06-17 13:33:48 +0900

    ScoreModelクラスを追加

---
+++ DogCatch(ver1.01)/ScoreModel.m
@@ -0,0 +1,13 @@
+//
+//  ScoreModel.m
+//  StrayDogOverOne
+//
+//  Created by RYO on 2015/06/17.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import "ScoreModel.h"
+
+@implementation ScoreModel
+
+@end

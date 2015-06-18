9272b88e2605b1203ba03b69cf97a92bcd78a87d
RYO <miniature-dachs@excite.co.jp>
2015-06-18 15:38:38 +0900

    AudioPlayerClassの生成

---
+++ DogCatch(ver1.01)/AudioPlayerClass.h
@@ -0,0 +1,13 @@
+//
+//  AudioPlayerClass.h
+//  StrayDogOverOne
+//
+//  Created by RYO on 2015/06/18.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioPlayerClass : NSObject
+
+@end
---
+++ DogCatch(ver1.01)/AudioPlayerClass.m
@@ -0,0 +1,13 @@
+//
+//  AudioPlayerClass.m
+//  StrayDogOverOne
+//
+//  Created by RYO on 2015/06/18.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import "AudioPlayerClass.h"
+
+@implementation AudioPlayerClass
+
+@end

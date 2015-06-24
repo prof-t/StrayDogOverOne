f071a627a3cb64588017d5a3b6ba0e3784fac71b
RYO <miniature-dachs@excite.co.jp>
2015-06-24 16:03:22 +0900

    PlistLoadクラスの新規作成 / ←のAppDelegateでの呼び出しを追記

---
+++ DogCatch(ver1.01)/Models/PlistLoad.m
@@ -0,0 +1,47 @@
+//
+//  PlistLoad.m
+//  StrayDogOverOne
+//
+//  Created by RYO on 2015/06/24.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import "PlistLoad.h"
+
+
+@implementation PlistLoad
+
++ (void)loadPlist
+{
+    //plistをロードする
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"SDPreloadingAudioFiles" ofType:@"plist"];
+    NSArray *plists = [NSArray arrayWithContentsOfFile:path];
+    
+    //plistの中身を表示
+    for (NSDictionary *audioInfo in plists){
+        //音声ファイル名
+        NSString *fileName = audioInfo[@"fileName"];
+        //key名
+        NSString *key = audioInfo[@"key"];
+        
+        //SDAudioPlayerManagerで音声ファイルをロード
+        [[SDAudioPlayerManager sharedInstance] createPlayerWithFileName:fileName forKey:key];
+        
+        NSLog(@"---------------");
+        NSLog(@"fileName : %@",fileName);
+        NSLog(@"key : %@",key);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+@end

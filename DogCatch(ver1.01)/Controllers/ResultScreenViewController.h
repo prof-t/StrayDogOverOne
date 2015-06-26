e530134460d2eb4da50a6b00dc64a62824af46a1
平山　亮 <hirayama16@beaa-no-MacBook-Air.local>
2015-06-26 09:10:36 +0900

    結果画面を表示するResultScreenViewControllerのクラスを作成

---
+++ DogCatch(ver1.01)/Controllers/ResultScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultScreenViewController.h
+//  StrayDogOverOne
+//
+//  Created by 平山　亮 on 2015/06/26.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ResultScreenViewController : BaseViewController
+
+@end

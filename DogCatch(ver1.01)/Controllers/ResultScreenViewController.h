e7674eb9a73bce92196c67d39ca8a61eb1fe5d12
平山　亮 <hirayama16@beaa-no-MacBook-Air.local>
2015-09-07 07:48:25 +0900

    resultViewController追加

---
+++ DogCatch(ver1.01)/Controllers/ResultScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultScreenViewController.h
+//  StrayDogOverOne
+//
+//  Created by 平山　亮 on 2015/09/06.
+//  Copyright (c) 2015年 RYO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultScreenViewController : UIViewController
+
+@end
